e99f04555227f891c8db69d460f87b0aabe78f0d
Romain Warnan <romain.warnan@insee.fr>
2018-09-07 09:21:41 +0200

    Méthode d'affichage de la date en français

---
+++ src/main/java/fr/insee/bar/date/Dates.java
@@ -0,0 +1,14 @@
+package fr.insee.bar.date;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Dates {
+
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE d MMMM yyyy", Locale.FRENCH);
+
+	public static String today() {
+		return formatter.format(LocalDate.now());
+	}
+}
