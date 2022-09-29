071edcc1995209ba6491001143c36db244bf4a2a
Stéphane Gamba <stephane.gamba@insee.fr>
2022-09-29 15:16:24 +0200

    correctif coctail l

--- src/main/java/fr/insee/bar/repository/CocktailSpec.java
+++ src/main/java/fr/insee/bar/repository/CocktailSpec.java
@@ -8,7 +8,8 @@ import fr.insee.bar.search.Search;
 public abstract class CocktailSpec {
 
 	public static Specification<Cocktail> search(String search) {
-		String q = "%" + Search.normalize(search) + " %";
-		return (cocktail, query, builder) -> builder.like(cocktail.get("nomNorm"), q);
+		 String q = "%" + Search.normalize(search) + "%";
+		return (entity, query, builder) -> builder.like(entity.get("nomNorm"), q);
+
 	}
 }
